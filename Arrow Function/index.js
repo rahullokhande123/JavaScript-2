0a354dc5e1afbe6207786f5ac6619f86ef46b979
Rahul Lokhande <rlokhande06244@gmail.com>
2024-06-29 23:51:41 +0530

    Arrow-Function-In-JS

---
+++ Arrow Function/index.js
@@ -0,0 +1,30 @@
+
+//=================== Arrow Function ===================
+
+let c=(a,b)=>{
+    return a+b;
+};
+console.log(c(5,5));
+
+//================= Arrow without argument ===============
+let y=()=>{
+    return 'Q';
+};
+console.log(y());
+
+// ================= Seconde Syntex of Arrow F() ===========
+// yah return hota h pr , esme hme return nhi likhna hota h
+let o=(a)=>(a*a)
+console.log(o(56))
+
+//========================================
+let i=(z)=>z
+console.log(i(3));
+
+//=======================================
+let p=k =>k
+console.log(p(90));
+
+
+
+
